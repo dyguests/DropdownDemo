81f0f24e1fdad40675e701293aba4e83d9db5329
fanhl <fanhonglin@klicen.com>
2018-04-03 14:27:49 +0800

    PopupWindow 相对居中处理

--- app/src/main/java/com/fanhl/dropdowndemo/MainActivity.kt
+++ app/src/main/java/com/fanhl/dropdowndemo/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.fanhl.dropdowndemo
 
 import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.widget.PopupWindowCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupWindow
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.popup_vehicle_picker.*
 import kotlinx.android.synthetic.main.popup_vehicle_picker.view.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,10 +24,15 @@ class MainActivity : AppCompatActivity() {
         btn_1.setOnClickListener {
             vehiclePicker.popup(btn_1)
         }
+
+        btn_2.setOnClickListener {
+            VehicleDialogFragment.popup(supportFragmentManager)
+        }
     }
 }
 
 class VehiclePicker {
+
     private var popupWindow: PopupWindow? = null
     fun popup(anchor: View) {
         if (popupWindow?.isShowing == true) {
@@ -42,7 +52,44 @@ class VehiclePicker {
             }
         }
 
-        popupWindow?.showAsDropDown(anchor)
+        //水平居中
+        //需要先测量，PopupWindow还未弹出时，宽高为0
+        popupWindow!!.contentView.measure(makeDropDownMeasureSpec(popupWindow!!.width), makeDropDownMeasureSpec(popupWindow!!.height))
+        val offsetX = (anchor.width - popupWindow!!.contentView.measuredWidth) / 2
+        val offsetY = 0
+
+        PopupWindowCompat.showAsDropDown(popupWindow!!, anchor, offsetX, offsetY, Gravity.START)
     }
 
+    private fun makeDropDownMeasureSpec(measureSpec: Int): Int {
+        val mode: Int
+        if (measureSpec == ViewGroup.LayoutParams.WRAP_CONTENT) {
+            mode = View.MeasureSpec.UNSPECIFIED
+        } else {
+            mode = View.MeasureSpec.EXACTLY
+        }
+        return View.MeasureSpec.makeMeasureSpec(View.MeasureSpec.getSize(measureSpec), mode)
+    }
+}
+
+class VehicleDialogFragment : DialogFragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.popup_vehicle_picker, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    companion object {
+        /** TAG */
+        private val TAG = VehicleDialogFragment::class.java.simpleName!!
+
+        fun popup(fragmentManager: FragmentManager) {
+            VehicleDialogFragment().show(fragmentManager, TAG)
+        }
+    }
 }
